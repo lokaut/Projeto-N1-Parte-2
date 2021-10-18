869dd3be92dfcb66f2a15fa92066cab84b0e1b1f
Gabrielfreitas11 <gabriel.freitas@smarkio.com.br>
2021-10-17 23:59:33 -0300

    subindo libs

--- functions/listUsers.py
+++ functions/listUsers.py
@@ -2,4 +2,9 @@ import pandas as pd
 
 def listUsers():
 
+    users = pd.read_excel('data/users.xlsx', sheet_name=None)
+
+    print("\nos usuários cadastrados são:\n")
+
+    print(users['users'])
     return
--- main.py
+++ main.py
@@ -5,15 +5,25 @@ from functions.deleteUser import deleteUser
 from functions.findUser import findUser
 from functions.listOrderUsers import listOrderUsers
 
-listOptions = [
-    "1 - Adicionar usuário",
-    "2 - listar usuários por ordem de criacão",
-    "3 - listar usuários por ordem alfabetica",
-    "4 - verificar se o usuário existe na lista",
-    "5 - remover usuário cadastrado",
-    "6 - atualizar usuário cadastrado",
-    "7 - encerrar a sessão"
+import inquirer
+
+options = [
+    'Adicionar usuário',
+    'listar usuários por ordem de criacão',
+    'listar usuários por ordem alfabetica',
+    'verificar se o usuário existe na lista', 
+    'remover usuário cadastrado', 
+    'atualizar usuário cadastrado',
+    'encerrar a sessão'
 ]
+
+questions = [
+    inquirer.List('option',
+                message="Sobre qual dos assuntos deseja tratar?",
+                choices=options,
+            ),
+]
+
 
 selectFuntions = {
     "1": addUser,
@@ -21,22 +31,29 @@ selectFuntions = {
     "3": listOrderUsers,
     "4": findUser,
     "5": deleteUser,
-    "6": updateUser,
+    "6": updateUser
 }
 
 def main():
+
+
     while True:
-        for x in listOptions:
-            print(x)
 
-        selectOption = int(input("\nSobre qual dos assuntos deseja tratar? "))
+        answers = inquirer.prompt(questions)
 
-        if selectOption > 0 and selectOption <= 6:
-            selectFuntions[str(selectOption)]
-        elif selectOption == 7:
+        index = options.index(answers['option'])+1
+
+
+        if index > 0 and index <= 6:
+            selectFuntions[str(index)]()
+        elif index == 7:
+            print('Certo, vou encerrar a sua conversa! Até logo!')
             break
-        else:
-            print("\nPor favor, digite um valor válido da lista \n")
+
+
+
+
+
 
 
 if(__name__ =="__main__"):
